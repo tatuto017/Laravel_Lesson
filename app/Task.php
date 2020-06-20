4e9a5406e9f4315eb665e1c0a419d25bb74c1d25
tatuto017 <tatuto017@gmail.com>
2020-06-20 11:14:33 +0900

    Chapter4終了

--- app/Task.php
+++ app/Task.php
@@ -3,8 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
-class Task extends Model
-{
-    //
+class Task extends Model {
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了'  , 'class' => '' ],
+    ];
+
+    public function getStatusLabelAttribute() {
+        $status = $this->attributes['status'];
+
+        if ( !isset(self::STATUS[$status]) ) {
+            return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
+
+    public function getStatusClassAttribute() {
+        $status = $this->attributes['status'];
+
+        if ( !isset(self::STATUS[$status]) ) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
+
+    public function getFormatDueDateAttribute() {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])->format('Y/m/d');
+    }
 }
