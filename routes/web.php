e7ec6e08b671a3e8fb6e64c5e9c9df4fa1649f32
tatuto017 <tatuto017@gmail.com>
2020-06-22 07:25:34 +0900

    ルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,10 @@
 |
 */
 
+// TOPアクセスはフォルダ1にリダイレクトしておく
+Route::redirect('/', '/folders/1/tasks');
+
+
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks,index');
+Route::get('/folders/create'    , 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create'   , 'FolderController@create');
