77688dfba9e9de1ad096bcd7cbc3cde9bef9ceed
tatuto017 <tatuto017@gmail.com>
2020-06-28 11:18:16 +0900

    ユーザー登録機能を実装

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,7 @@
 |
 */
 
-// TOPアクセスはフォルダ1にリダイレクトしておく
-Route::redirect('/', '/folders/1/tasks');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create'    , 'FolderController@showCreateForm')->name('folders.create');
@@ -23,3 +22,5 @@ Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
 Route::get('/folders/{id}/tasks/{task_id}/edit'  , 'TaskController@showEditForm')->name('tasks.edit');
 Route:: post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+Auth::routes();
