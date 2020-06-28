4e2a5b72f5cb9fd7599ea62346ccc57f465c20db
tatuto017 <tatuto017@gmail.com>
2020-06-28 09:56:22 +0900

    テーブルのmigrateとテストデータの作成

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -10,11 +10,14 @@ class FoldersTableSeeder extends Seeder {
      * @return void
      */
     public function run() {
+        $user = DB::table('users')->first();
+
         $tities = ['プライベート', '仕事', '旅行'];
 
         foreach ($tities as $title) {
             DB::table('folders')->insert([
-                'title' => $title,
+                'title'      => $title,
+                'user_id'    => $user->id,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ]);
